01d0788aca78d67cabbc5db4c9b14498e1484e9e
AYŞE ALEYNA YAVUZ <192802024@ogr.cbu.edu.tr>
2021-02-25 22:51:02 +0300

    22.50

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    //temel voidler için
+    public interface IResult
+    {
+        bool Success { get; }  //işlem başarılı mı? başarısız mı?
+        string Message { get; } //işlem başarılı ya da başarısız bilgilendirmesi
+
+    }
+}
