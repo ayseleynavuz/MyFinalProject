ec73448a45b3e6e56b0de1adaceec71b84303a31
AYŞE ALEYNA YAVUZ <192802024@ogr.cbu.edu.tr>
2021-02-26 03:55:05 +0300

    26.02.2021

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,7 +9,7 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün adı geçersiz";
-
-
+        internal static string ProductsListed;
+        internal static List<Product> MaintenanceTime;
     }
 }
